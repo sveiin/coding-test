2a2eee03a71e75cb2f30767648402438591ced69
sveiin <163740157+sveiin@users.noreply.github.com>
2024-05-04 23:10:48 +0900

    [Bronze II] Title: 단어의 개수, Time: 516 ms, Memory: 37924 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine().trim();
+        
+        if(s.isEmpty()) {
+            System.out.println(0);
+        } else {
+            String[] str = s.split(" ");
+            System.out.println(str.length);
+        }
+    }
+}
