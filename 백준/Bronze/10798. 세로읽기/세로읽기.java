a8a1aec33a208b25f5d458899fab253a6d6882d4
sveiin <163740157+sveiin@users.noreply.github.com>
2024-05-15 19:44:01 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        char[][] arr = new char[5][];
+        
+        int maxIndex = 0;
+        for(int i = 0; i < arr.length; i++) {
+            String s = br.readLine();
+            int len = s.length();
+            
+            if(len > maxIndex) {
+                maxIndex = len;
+            }
+            
+            arr[i] = new char[len];
+            
+            for(int j = 0; j < len; j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+        
+        for(int i = 0; i < maxIndex; i++) {
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[j].length <= i) {
+                    continue;
+                }
+                
+                System.out.print(arr[j][i]);
+            }
+        }
+        
+        br.close();
+    }
+}
