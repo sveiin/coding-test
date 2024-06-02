f02799f052611bed508602d6866f42ab64def3d5
sveiin <163740157+sveiin@users.noreply.github.com>
2024-06-02 21:51:17 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result;
+        
+        if (num1 == num2) {
+            result = 1;
+        } else {
+            result = -1;
+        }
+        
+        return result;
+    }
+}
