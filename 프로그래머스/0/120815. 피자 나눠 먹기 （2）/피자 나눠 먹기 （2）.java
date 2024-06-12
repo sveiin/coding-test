aed68f5f66970d6837a0bec1d68a254e6bb16a5a
sveiin <163740157+sveiin@users.noreply.github.com>
2024-06-12 22:42:31 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int count = 1;
+        
+        while ((6 * count) % n != 0 ) {
+            count++;
+        }
+        
+        return count;
+    }
+}
