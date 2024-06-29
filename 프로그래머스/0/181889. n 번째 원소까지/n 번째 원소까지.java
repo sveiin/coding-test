9a39b9693ef854588e0da0b5cae16231a3bbf69e
sveiin <163740157+sveiin@users.noreply.github.com>
2024-06-29 22:18:55 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers, int n) {
+        int[] result = Arrays.copyOfRange(numbers, 0, n);
+        
+        return result;
+    }
+}
