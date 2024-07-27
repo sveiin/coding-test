7b49bc4489b49a799b126402141117a7a79fae5d
sveiin <163740157+sveiin@users.noreply.github.com>
2024-07-27 21:24:46 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.84 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            
+            for (int j = start; j <= end; j++) {
+                arr[j] += 1;
+            }
+        }
+        
+        return arr;
+    }
+}
