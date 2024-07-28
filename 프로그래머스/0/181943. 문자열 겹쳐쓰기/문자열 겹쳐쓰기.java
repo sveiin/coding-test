393aef93dfe3be7f4454d6264b6a800c7a9d7e69
sveiin <163740157+sveiin@users.noreply.github.com>
2024-07-28 14:54:22 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.13 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,9 +2,9 @@ class Solution {
     public String solution(String str1, String str2, int s) {
         StringBuilder result = new StringBuilder();
         
-        result.append(str1.substring(0, s));
+        result.append(str1, 0, s);
         result.append(str2);
-        result.append(str1.substring(s + str2.length()));
+        result.append(str1, s + str2.length(), str1.length());
         
         return result.toString();
     }
