963a95d128ae9f2e74b151cf59e218e49f7282b5
sveiin <163740157+sveiin@users.noreply.github.com>
2024-08-10 23:22:05 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        int result = n;
+        
+        for (int i = 0; i < t; i++) {
+            result *= 2;
+        }
+        
+        return result;
+    }
+}
