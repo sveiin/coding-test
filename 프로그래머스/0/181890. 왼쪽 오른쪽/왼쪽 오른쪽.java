52c44bc89619fd8b8fa699b782d50d59ed306d19
sveiin <163740157+sveiin@users.noreply.github.com>
2024-10-04 23:41:08 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -4,11 +4,11 @@ class Solution {
     public String[] solution(String[] strs) {
 
         for (int i = 0; i < strs.length; i++) {
-            if (strs[i].charAt(0) == 'l') {
+            if (strs[i].equals("l")) {
                 return Arrays.copyOfRange(strs, 0, i);
             }
             
-            if (strs[i].charAt(0) == 'r') {
+            if (strs[i].equals("r")) {
                 return Arrays.copyOfRange(strs, i + 1, strs.length);
             }
         }
