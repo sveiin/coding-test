d612b2f85a0411e8a24b70d5af18b088b7aafbb3
sveiin <163740157+sveiin@users.noreply.github.com>
2024-11-28 15:08:07 +0900

    [level 1] Title: 기사단원의 무기, Time: 136.64 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    count += (i / j == j) ? 1 : 2;
+                }
+            }
+            
+            answer += (count > limit) ? power : count;
+        }
+        
+        return answer;
+    }
+}
